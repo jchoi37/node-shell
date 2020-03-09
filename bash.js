26cb7308c0054bf4b676004a14fcca47b0318323
jchoi37 <j@users-MacBook.local>
2020-03-09 13:13:02 -0400

    started pwd

--- bash.js
+++ bash.js
@@ -1,9 +1,14 @@
 process.stdout.write('prompt > ');
 
-process.stdin.on('data',(data)=>{
+/* process.stdin.on('data',(data)=>{
   const cmd= data.toString().trim();
 
   process.stdout.write("You typed: " + cmd);
   process.stdout.write('\nprompt > ')
 
+})
+ */
+
+process.stdin.on('pwd', function(){
+  console.log(`Current directory: ${process.cwd()}`); 
 })
